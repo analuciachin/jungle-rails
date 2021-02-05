e6b4374f34f8d8285c1881c3ec2b826d365047e3
analuciachin <ananime@gmail.com>
2021-02-05 20:24:49 +0000

    work on user authentication feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,24 @@
 Rails.application.routes.draw do
 
+  #root to: 'gif#cool'
   root to: 'products#index'
   get '/about' => 'about#index'
+
+  # These routes will be for signup. The first renders a form in the browse, the second will 
+  # receive the form and create a user in our database using the data given to us by the user.
+
+  get '/cool' => 'gif#cool'
+  get '/sweet' => 'gif#sweet'
+
+  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
